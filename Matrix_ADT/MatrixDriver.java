0e26846d49fcea8893b193d6bbb87c55195b414c
Oscar Pinedo <opinedo19@gmail.com>
2018-06-09 19:56:56 -0700

    added Makefile and initial test class for matrix implementation

---
+++ Matrix_ADT/MatrixDriver.java
@@ -0,0 +1,10 @@
+/*  Tests all procedures for our Matrix class for a sparse matrix 
+	implementation. 
+*/
+
+class MatrixDriver{
+
+	public static void main(String args[]){
+		System.out.println("Beggining of driver test.");
+	}
+}
